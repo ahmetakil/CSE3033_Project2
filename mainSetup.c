6de5b95a61b336fa251b87131f83b48574510401
Fatih Emin Öge <fatiheminoge@gmail.com>
2020-12-30 10:57:24 +0000

    & li çalıştırınca line kayması oluyor

--- mainSetup.c
+++ mainSetup.c
@@ -4,8 +4,6 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <dirent.h>
-#include <stdbool.h>
 #include "utils.c"
 
 #define MAX_LINE 80 /* 80 chars per line, per command, should be enough. */
@@ -15,15 +13,11 @@ in the next command line; separate it into distinct arguments (using blanks as
 delimiters), and set the args array entries to point to the beginning of what
 will become null-terminated, C-style strings. */
 
-extern char *findPath(char *);
-extern void remove_element(char *, int , int );
-
-void setup(char inputBuffer[], char *args[], int *background)
-{
+void setup(char inputBuffer[], char *args[], int *background) {
     int length, /* # of characters in the command line */
-        i,      /* loop index for accessing inputBuffer array */
-        start,  /* index where beginning of next command parameter is */
-        ct;     /* index of where to place the next parameter into args[] */
+    i,      /* loop index for accessing inputBuffer array */
+    start,  /* index where beginning of next command parameter is */
+    ct;     /* index of where to place the next parameter into args[] */
 
     ct = 0;
 
@@ -44,88 +38,79 @@ void setup(char inputBuffer[], char *args[], int *background)
     /* if the process is in the read() system call, read returns -1
   However, if this occurs, errno is set to EINTR. We can check this  value
   and disregard the -1 value */
-    if ((length < 0) && (errno != EINTR))
-    {
+    if ((length < 0) && (errno != EINTR)) {
         perror("error reading the command");
         exit(-1); /* terminate with error code of -1 */
     }
 
-    for (i = 0; i < length; i++)
-    { /* examine every character in the inputBuffer */
+    for (i = 0; i < length; i++) { /* examine every character in the inputBuffer */
 
-        switch (inputBuffer[i])
-        {
-        case ' ':
-        case '\t': /* argument separators */
-            if (start != -1)
-            {
-                args[ct] = &inputBuffer[start]; /* set up pointer */
-                ct++;
-            }
-            inputBuffer[i] = '\0'; /* add a null char; make a C string */
-            start = -1;
-            break;
+        switch (inputBuffer[i]) {
+            case ' ':
+            case '\t': /* argument separators */
+                if (start != -1) {
+                    args[ct] = &inputBuffer[start]; /* set up pointer */
+                    ct++;
+                }
+                inputBuffer[i] = '\0'; /* add a null char; make a C string */
+                start = -1;
+                break;
 
-        case '\n': /* should be the final char examined */
-            if (start != -1)
-            {
-                args[ct] = &inputBuffer[start];
-                ct++;
-            }
-            inputBuffer[i] = '\0';
-            args[ct] = NULL; /* no more arguments to this command */
-            break;
+            case '\n': /* should be the final char examined */
+                if (start != -1) {
+                    args[ct] = &inputBuffer[start];
+                    ct++;
+                }
+                inputBuffer[i] = '\0';
+                args[ct] = NULL; /* no more arguments to this command */
+                break;
 
-        default: /* some other character */
-            if (start == -1)
-                start = i;
-            if (inputBuffer[i] == '&')
-            {
-                *background = 1;
-                inputBuffer[i - 1] = '\0';
-            }
+            default: /* some other character */
+                if (start == -1)
+                    start = i;
+                if (inputBuffer[i] == '&') {
+                    *background = 1;
+                    inputBuffer[i - 1] = '\0';
+                }
         }            /* end of switch */
     }                /* end of for */
     args[ct] = NULL; /* just in case the input line was > 80 */
 
 } /* end of setup routine */
 
-int main(void)
-{
+int main(void) {
     setvbuf(stdout, NULL, _IONBF, 0);
     pid_t child_pid;
     char inputBuffer[MAX_LINE];   /*buffer to hold command entered */
     int background;               /* equals 1 if a command is followed by '&' */
     char *args[MAX_LINE / 2 + 1]; /*command line arguments */
-    char *paths = getenv("PATH");
-    while (1)
-    {
+    while (1) {
         background = 0;
         printf("myshell: ");
         /*setup() calls exit() when Control-D is entered */
         setup(inputBuffer, args, &background);
+        build_command(args);
         char *path = findPath(args[0]);
         child_pid = fork();
-        if (child_pid == -1)
-        {
+        if (child_pid == -1) {
             perror("Failed to fork");
             return 1;
         }
-        if (child_pid == 0)
-        {
+        if (child_pid == 0) {
             execv(path, &args[0]);
-        }
-        switch (background)
-        {
-        case 0:
-            wait(NULL);
-            break;
-        case 1:
-            waitpid(-1,NULL,WNOHANG);
-            break;
+        } else {
+            switch (background) {
+                case 0:
+                    wait(NULL);
+                    break;
+                case 1:
+                    waitpid(-1, NULL, WNOHANG);
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
 
 }
-
--- utils.c
+++ utils.c
@@ -4,28 +4,26 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define MAX_LINE 80
+
 char *concat(const char *, const char *);
 
-char *findPath(char *command)
-{
-    char *paths; 
+char *findPath(char *command) {
+    char *paths;
     paths = getenv("PATH");
     // şurda size ı düzegün alamadım
     int length = strlen(paths);
     char *temp = malloc(length);
-    memcpy(temp,paths,length);
+    memcpy(temp, paths, length);
     char *token;
 
     DIR *d;
     struct dirent *dir;
     bool finded = false;
-    while ((token = strtok_r(temp, ":", &temp)))
-    {
+    while ((token = strtok_r(temp, ":", &temp))) {
         d = opendir(token);
-        while ((dir = readdir(d)) != NULL)
-        {
-            if (strcmp(dir->d_name, command) == 0)
-            {
+        while ((dir = readdir(d)) != NULL) {
+            if (strcmp(dir->d_name, command) == 0) {
                 finded = true;
                 break;
             }
@@ -34,15 +32,13 @@ char *findPath(char *command)
             break;
     }
 
-    char *path = concat("/",command);
-    path = concat(token,path);
+    char *path = concat("/", command);
+    path = concat(token, path);
 
     return path;
 }
 
-
-char *concat(const char *s1, const char *s2)
-{
+char *concat(const char *s1, const char *s2) {
     char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
     // in real code you would check for errors in malloc here
     strcpy(result, s1);
@@ -50,9 +46,14 @@ char *concat(const char *s1, const char *s2)
     return result;
 }
 
-void remove_element(char *array, int index, int array_length)
-{
-    int i;
-    for (i = index; i < array_length - 1; i++)
-        array[i] = array[i + 1];
+void build_command(char *args[]) {
+    int i = 0;
+    while (args[i] != NULL) {
+        if (strcmp(args[i], "&") == 0) {
+            int j;
+            for (j = i; j < sizeof(args); j++)
+                args[j] = args[j + 1];
+        }
+        i++;
+    }
 }
