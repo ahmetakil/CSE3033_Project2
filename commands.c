f76c2488035944604c1b39b194ff195d7fa9af44
Fatih Emin Öge <fatiheminoge@gmail.com>
2020-12-30 19:39:16 +0000

    soru 2 ps_all bitti linux da test yapılmadı

---
+++ commands.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include "utils.c"
+
+const char *COMMANDS[] = {"ps_all", "search", "bookmark", "exit"};
+
+int check_command(char *command)
+{
+    int len = sizeof(COMMANDS) / sizeof(*COMMANDS);
+    int i;
+
+    for (i = 0; i < len; i++)
+    {
+        if (strcmp(COMMANDS[i], command) == 0)
+            return i + 1;
+    }
+    return 0;
+}
+
+void ps_all(struct process **background, struct process **finished, int *jobNumber)
+{
+    printf("Running:\n");
+    struct process *iterback = *background;
+    while (iterback != NULL)
+    {
+        printf("\t[%d] %s (Pid=%d)\n", iterback->jobNumber, iterback->processName, (int)iterback->pid);
+        iterback = iterback->next;
+    }
+    if (*background == NULL)
+        *jobNumber = 1;
+    printf("Finished:\n");
+    struct process *iterfin = *finished;
+    while (iterfin != NULL)
+    {
+        printf("\t[%d] %s\n", iterfin->jobNumber, iterfin->processName);
+        iterfin = iterfin->next;
+    }
+    deleteList(finished);
+}
+
+void run_command(int index, struct process **finished_pids, struct process **background_pids, int *jobNumber)
+{
+    switch (index)
+    {
+    case 1:
+        ps_all(finished_pids, background_pids, jobNumber);
+        break;
+
+    default:
+        break;
+    }
+}
--- mainSetup.c
+++ mainSetup.c
@@ -4,7 +4,7 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <string.h>
-#include "utils.c"
+#include "commands.c"
 
 #define MAX_LINE 80 /* 80 chars per line, per command, should be enough. */
 
@@ -13,11 +13,12 @@ in the next command line; separate it into distinct arguments (using blanks as
 delimiters), and set the args array entries to point to the beginning of what
 will become null-terminated, C-style strings. */
 
-void setup(char inputBuffer[], char *args[], int *background) {
+void setup(char inputBuffer[], char *args[], int *background)
+{
     int length, /* # of characters in the command line */
-    i,      /* loop index for accessing inputBuffer array */
-    start,  /* index where beginning of next command parameter is */
-    ct;     /* index of where to place the next parameter into args[] */
+        i,      /* loop index for accessing inputBuffer array */
+        start,  /* index where beginning of next command parameter is */
+        ct;     /* index of where to place the next parameter into args[] */
 
     ct = 0;
 
@@ -38,79 +39,114 @@ void setup(char inputBuffer[], char *args[], int *background) {
     /* if the process is in the read() system call, read returns -1
   However, if this occurs, errno is set to EINTR. We can check this  value
   and disregard the -1 value */
-    if ((length < 0) && (errno != EINTR)) {
+    if ((length < 0) && (errno != EINTR))
+    {
         perror("error reading the command");
         exit(-1); /* terminate with error code of -1 */
     }
 
-    for (i = 0; i < length; i++) { /* examine every character in the inputBuffer */
+    for (i = 0; i < length; i++)
+    { /* examine every character in the inputBuffer */
 
-        switch (inputBuffer[i]) {
-            case ' ':
-            case '\t': /* argument separators */
-                if (start != -1) {
-                    args[ct] = &inputBuffer[start]; /* set up pointer */
-                    ct++;
-                }
-                inputBuffer[i] = '\0'; /* add a null char; make a C string */
-                start = -1;
-                break;
+        switch (inputBuffer[i])
+        {
+        case ' ':
+        case '\t': /* argument separators */
+            if (start != -1)
+            {
+                args[ct] = &inputBuffer[start]; /* set up pointer */
+                ct++;
+            }
+            inputBuffer[i] = '\0'; /* add a null char; make a C string */
+            start = -1;
+            break;
 
-            case '\n': /* should be the final char examined */
-                if (start != -1) {
-                    args[ct] = &inputBuffer[start];
-                    ct++;
-                }
-                inputBuffer[i] = '\0';
-                args[ct] = NULL; /* no more arguments to this command */
-                break;
+        case '\n': /* should be the final char examined */
+            if (start != -1)
+            {
+                args[ct] = &inputBuffer[start];
+                ct++;
+            }
+            inputBuffer[i] = '\0';
+            args[ct] = NULL; /* no more arguments to this command */
+            break;
 
-            default: /* some other character */
-                if (start == -1)
-                    start = i;
-                if (inputBuffer[i] == '&') {
-                    *background = 1;
-                    inputBuffer[i - 1] = '\0';
-                }
+        default: /* some other character */
+            if (start == -1)
+                start = i;
+            if (inputBuffer[i] == '&')
+            {
+                *background = 1;
+                inputBuffer[i - 1] = '\0';
+            }
         }            /* end of switch */
     }                /* end of for */
     args[ct] = NULL; /* just in case the input line was > 80 */
 
 } /* end of setup routine */
 
-int main(void) {
+int main(void)
+{
     setvbuf(stdout, NULL, _IONBF, 0);
     pid_t child_pid;
     char inputBuffer[MAX_LINE];   /*buffer to hold command entered */
     int background;               /* equals 1 if a command is followed by '&' */
     char *args[MAX_LINE / 2 + 1]; /*command line arguments */
-    while (1) {
+    int jobNumber = 1;
+    struct process *background_pids = createProcess();
+    struct process *finished_pids = createProcess();
+    while (1)
+    {
         background = 0;
         printf("myshell: ");
         /*setup() calls exit() when Control-D is entered */
         setup(inputBuffer, args, &background);
-        build_command(args);
+        build_command(args); // args without &
+
+        char *command = args[0];
+        char *processName = getProcessName(args);
         char *path = findPath(args[0]);
-        child_pid = fork();
-        if (child_pid == -1) {
+
+        int commandIndex = check_command(command);
+        if (!commandIndex && path == NULL)
+        {
+            perror(("%s is not a valid command", command));
+            continue;
+        }
+        if (!commandIndex)
+            child_pid = fork();
+        if (child_pid == -1)
+        {
             perror("Failed to fork");
             return 1;
         }
-        if (child_pid == 0) {
+        if (child_pid == 0 && path != NULL)
+        {
             execv(path, &args[0]);
-        } else {
-            switch (background) {
-                case 0:
-                    wait(NULL);
-                    break;
-                case 1:
-                    waitpid(-1, NULL, WNOHANG);
-                    break;
-                default:
-                    break;
+        }
+        else
+        {
+            if (commandIndex)
+            {
+                run_command(commandIndex, &background_pids, &finished_pids, &jobNumber);
             }
         }
 
+        switch (background)
+        {
+        case 0:
+            child_pid = wait(NULL);
+            break;
+        case 1:
+            child_pid = waitpid(-1, NULL, WNOHANG);
+            if (child_pid != 0)
+                insertPid(&background_pids, child_pid, processName, &jobNumber);
+            else
+            {
+                deletePid(&background_pids, child_pid, &jobNumber);
+                insertPid(&finished_pids, child_pid, processName, &jobNumber);
+            }
+            break;
+        }
     }
-
 }
